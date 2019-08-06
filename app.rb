7d66654fb247587fe8ea19577160a06fd5ba3e72
tomoaki-kimura <tkim19750404@gmail.com>
2019-08-06 09:44:17 +0900

    fix_return_word

--- app.rb
+++ app.rb
@@ -8,7 +8,15 @@ end
 post '/' do
   input = params['keyword']
   # ここにコードを入力してください。
-
+  if input == "おはよう"
+    @greet = "オハヨウ"
+  elsif input == "こんにちは"
+    @greet = "コンニチワ"
+  elsif input == "こんばんは"
+    @greet = "コンバンワ"
+  else 
+    @greet = "リカイデキマセン"
+  end
 
   # ここまでコードを入力してね。
   erb :index
